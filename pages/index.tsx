ef4341423d1c2c7977fbe6bc0d58f4882ae0ddaa
Adivaprayoga <prayoga.adiva@gmail.com>
2022-09-21 16:41:02 +0800

    Added Auth for github

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import { supabase } from '../utils/supabase'
 import Link from "next/link";
 
 export default function Home({ lessons }) {
-  console.log(lessons)
+  console.log(supabase.auth.user())
   return (
     <div className="w-full max-w-3xl mx-auto my-16 px-2">
       {lessons.map((lesson) => (
